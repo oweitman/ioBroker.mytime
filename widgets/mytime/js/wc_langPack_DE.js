a16201ca713863d5bc0a82ea47b1bd9360c90922
oweitman <oweitman@gmx.de>
2024-11-11 19:07:26 +0100

    fix german EIN Uhr #379

--- widgets/mytime/js/wc_langPack_DE.js
+++ widgets/mytime/js/wc_langPack_DE.js
@@ -10,17 +10,17 @@ WordClock LanguagePack DE
 vis.binds['mytime'].wordclock.addLanguage({
   langCode: 'DE',
   letterSet: [
-    ["E","S","T","I","S","T","I","M","E","I","N","E"],
-    ["V","I","E","R","T","E","L","A","Z","E","H","N"],
-    ["Z","W","A","N","Z","I","G","F","Ü","N","F","N"],
-    ["N","A","C","H","V","O","R","D","H","A","L","B"],
-    ["D","A","T","Z","W","Ö","L","F","E","I","N","S"],
-    ["Z","W","E","I","D","R","E","I","V","I","E","R"],
-    ["F","Ü","N","F","S","E","C","H","S","E","L","F"],
-    ["E","S","I","E","B","E","N","A","C","H","T","T"],
-    ["N","E","U","N","I","Z","E","H","N","U","H","R"]
+    ["E", "S", "T", "I", "S", "T", "I", "M", "E", "I", "N", "E"],
+    ["V", "I", "E", "R", "T", "E", "L", "A", "Z", "E", "H", "N"],
+    ["Z", "W", "A", "N", "Z", "I", "G", "F", "Ü", "N", "F", "N"],
+    ["N", "A", "C", "H", "V", "O", "R", "D", "H", "A", "L", "B"],
+    ["D", "A", "T", "Z", "W", "Ö", "L", "F", "E", "I", "N", "S"],
+    ["Z", "W", "E", "I", "D", "R", "E", "I", "V", "I", "E", "R"],
+    ["F", "Ü", "N", "F", "S", "E", "C", "H", "S", "E", "L", "F"],
+    ["E", "S", "I", "E", "B", "E", "N", "A", "C", "H", "T", "T"],
+    ["N", "E", "U", "N", "I", "Z", "E", "H", "N", "U", "H", "R"]
   ],
-  timeString: function(h, m, settings = { round: false }) {
+  timeString: function (h, m, settings = { round: false }) {
     var ret = 'ES IST ';
     h %= 12;
     if (h == 0) h = 12;
@@ -39,43 +39,43 @@ vis.binds['mytime'].wordclock.addLanguage({
       'ZWÖLF '
     ];
     switch (
-      (settings.round ? Math.round(m / 5) * 5 : Math.floor(m / 5) * 5) % 60
+    (settings.round ? Math.round(m / 5) * 5 : Math.floor(m / 5) * 5) % 60
     ) {
       case 0:
-        ret += (h == 1 ? 'EIN ' : hourNames[h - 1]) + 'UHR ';
+        ret += (h == 1 ? 'EIN ' : hourNames[h - 1]) + 'UHR';
         break;
       case 5:
-        ret += 'FÜNF NACH ' + hourNames[h - 1]+"UHR ";
+        ret += 'FÜNF NACH ' + hourNames[h - 1];
         break;
       case 10:
-        ret += 'ZEHN NACH ' + hourNames[h - 1]+"UHR ";
+        ret += 'ZEHN NACH ' + hourNames[h - 1];
         break;
       case 15:
-        ret += 'VIERTEL NACH ' + hourNames[h - 1]+"UHR ";
+        ret += 'VIERTEL NACH ' + hourNames[h - 1];
         break;
       case 20:
-        ret += 'ZWANZIG NACH ' + hourNames[h - 1]+"UHR ";
+        ret += 'ZWANZIG NACH ' + hourNames[h - 1];
         break;
       case 25:
-        ret += 'FÜNF VOR HALB ' + hourNames[h % 12]+"UHR ";
+        ret += 'FÜNF VOR HALB ' + hourNames[h % 12];
         break;
       case 30:
-        ret += 'HALB ' + hourNames[h % 12]+"UHR ";
+        ret += 'HALB ' + hourNames[h % 12];
         break;
       case 35:
-        ret += 'FÜNF NACH HALB ' + hourNames[h % 12]+"UHR ";
+        ret += 'FÜNF NACH HALB ' + hourNames[h % 12];
         break;
       case 40:
-        ret += 'ZWANZIG VOR ' + hourNames[h % 12]+"UHR ";
+        ret += 'ZWANZIG VOR ' + hourNames[h % 12];
         break;
       case 45:
-        ret += 'VIERTEL VOR ' + hourNames[h % 12]+"UHR ";
+        ret += 'VIERTEL VOR ' + hourNames[h % 12];
         break;
       case 50:
-        ret += 'ZEHN VOR ' + hourNames[h % 12]+"UHR ";
+        ret += 'ZEHN VOR ' + hourNames[h % 12];
         break;
       case 55:
-        ret += 'FÜNF VOR ' + hourNames[h % 12]+"UHR ";
+        ret += 'FÜNF VOR ' + hourNames[h % 12];
         break;
     }
     return ret;
