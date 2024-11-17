d8184ca442daba1d507e14730ca9d9ec54cf3cda
oweitman <oweitman@gmx.de>
2024-11-17 19:26:31 +0100

    improve build process for widgets

---
+++ widgets/mytime/js/bundle.js
@@ -0,0 +1,13 @@
+
+import "./mytime.js";
+import "./wc_langPack_CH_BERN.js";
+import "./wc_langPack_DE.js";
+import "./wc_langPack_DE_SWG.js";
+import "./wc_langPack_EN.js";
+import "./wc_langPack_NL.js";
+import "./wc_langPack_IT.js";
+import "./wc_langPack_FR.js";
+import "./wc_langPack_RU.js";
+import "./wc_langPack_ES.js";
+import "./wc_langPack_TR.js";
+import "./timezones.js";
